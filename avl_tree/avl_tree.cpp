cdfce36ca40ebca40b2a9f3b22e5ffe0c109f49b
Marcos Vinicius <marcos.v.silva@live.com>
2019-05-29 17:27:26 -0300

    start avl tree implementation

---
+++ avl_tree/avl_tree.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+struct node{
+    int value;
+    node *father;
+    node *son;
+    node *brother;
+};
+
+int main(){
+    
+    
+    return 0;
+}
